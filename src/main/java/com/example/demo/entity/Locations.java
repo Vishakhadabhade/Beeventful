7911f908ab329e2eeb84975d0459ea24d32eb201
Vishakhadabhade <114911835+Vishakhadabhade@users.noreply.github.com>
2023-01-26 04:32:12 -0800

    Update Locations.java

--- src/main/java/com/example/demo/entity/Locations.java
+++ src/main/java/com/example/demo/entity/Locations.java
@@ -1,33 +1,42 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+
+@Data
 @Entity
-public class Locations {
+@AllArgsConstructor
+public class Decorators {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Long id;
-	private String name;
-	@Column(nullable=false , unique=true)
-	private String locationCode;
-	private String state;
-	private Long perDayRent;
 	
-	public Locations() {}
-
-	public Locations(Long id, String name, String locationCode, String state, Long perDayRent) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.locationCode = locationCode;
-		this.state = state;
-		this.perDayRent = perDayRent;
-	}
+	@NotBlank(message = "Name is required field")
+	private String name;
+	
+	@Column(nullable=false , unique=true)
+	private String decoratorsCode;
+	
+	private Long indoorDecor;
+	
+	private Long outdoorDecor;
+	
+	@OneToMany(mappedBy= "decorators" ,cascade = CascadeType.ALL)
+	List <Events> events ;
 
 	public Long getId() {
 		return id;
@@ -45,36 +54,37 @@ public class Locations {
 		this.name = name;
 	}
 
-	public String getLocationCode() {
-		return locationCode;
+	public String getDecoratorsCode() {
+		return decoratorsCode;
 	}
 
-	public void setLocationCode(String locationCode) {
-		this.locationCode = locationCode;
+	public void setDecoratorsCode(String decoratorsCode) {
+		this.decoratorsCode = decoratorsCode;
 	}
 
-	public String getState() {
-		return state;
+	public Long getIndoorDecor() {
+		return indoorDecor;
 	}
 
-	public void setState(String state) {
-		this.state = state;
+	public void setIndoorDecor(Long indoorDecor) {
+		this.indoorDecor = indoorDecor;
 	}
 
-	public Long getPerDayRent() {
-		return perDayRent;
+	public Long getOutdoorDecor() {
+		return outdoorDecor;
 	}
 
-	public void setPerDayRent(Long perDayRent) {
-		this.perDayRent = perDayRent;
+	public void setOutdoorDecor(Long outdoorDecor) {
+		this.outdoorDecor = outdoorDecor;
 	}
 
-	@Override
-	public String toString() {
-		return "Locations [id=" + id + ", name=" + name + ", locationCode=" + locationCode + ", state=" + state
-				+ ", perDayRent=" + perDayRent + "]";
+	public List<Events> getEvents() {
+		return events;
 	}
-	
+
+	public void setEvents(List<Events> events) {
+		this.events = events;
+	}
 	
 	
 	
